b09c1db42190dfe9653b16804187dd07aff9996e
Simon Schäfer <34800304+Eshco93@users.noreply.github.com>
2023-02-25 01:17:07 +0100

    Update threads.py
    * Added CRC check

--- SondeHubUploader/threads.py
+++ SondeHubUploader/threads.py
@@ -44,36 +44,40 @@ def process_aprs_queue(self):
             # Optionally write the raw data
             if self.writeo:
                 self.writeData.write_raw_data(self, aprs_package)
-            # Parse the APRS package
-            telemetry = self.handleData.parse_aprs_package(self, aprs_package)
-            self.loggerObj.debug('APRS package parsed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
-            self.loggerObj.info('Telemetry received (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
-            # Check whether the telemetry is plausible
-            telemetry = self.telemetryChecks.check_plausibility(self, telemetry)
-            self.loggerObj.debug('Plausibility checks performed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
-            # Optionally write the telemetry
-            if self.writet:
-                if 'serial' in telemetry:
-                    self.writeData.write_telemetry(self, telemetry)
+            # Perform CRC
+            if self.crc_calculator.verify(aprs_package[:-2], aprs_package[-1] << 8 | aprs_package[-2]):
+                # Parse the APRS package
+                telemetry = self.handleData.parse_aprs_package(self, aprs_package[:-2])
+                self.loggerObj.debug('APRS package parsed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
+                self.loggerObj.info('Telemetry received (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
+                # Check whether the telemetry is plausible
+                telemetry = self.telemetryChecks.check_plausibility(self, telemetry)
+                self.loggerObj.debug('Plausibility checks performed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
+                # Optionally write the telemetry
+                if self.writet:
+                    if 'serial' in telemetry:
+                        self.writeData.write_telemetry(self, telemetry)
+                    else:
+                        self.loggerObj.error('Could not write telemetry (serial missing)')
+                # Check whether the mandatory telemetry for SondeHub is included
+                if self.telemetryChecks.check_mandatory(self, telemetry):
+                    self.loggerObj.debug('Mandatory data check successful (Serial: %s)', telemetry['serial'])
+                    # Reformat the telemetry to the SondeHub telemetry format
+                    reformatted_telemetry = self.handleData.reformat_telemetry(self, telemetry)
+                    self.loggerObj.debug('Telemetry reformatted (Serial: %s)', reformatted_telemetry['serial'])
+                    # Optionally write the reformatted telemetry
+                    if self.writer:
+                        self.writeData.write_reformatted_telemetry(self, reformatted_telemetry)
+                    # Store the reformatted telemetry to the upload queue
+                    try:
+                        self.upload_queue.put(reformatted_telemetry, False)
+                        self.loggerObj.debug('Reformatted telemetry put in queue (Serial: %s)', reformatted_telemetry['serial'])
+                    except queue.Full:
+                        self.loggerObj.warning('Upload queue full')
                 else:
-                    self.loggerObj.error('Could not write telemetry (serial missing)')
-            # Check whether the mandatory telemetry for SondeHub is included
-            if self.telemetryChecks.check_mandatory(self, telemetry):
-                self.loggerObj.debug('Mandatory data check successful (Serial: %s)', telemetry['serial'])
-                # Reformat the telemetry to the SondeHub telemetry format
-                reformatted_telemetry = self.handleData.reformat_telemetry(self, telemetry)
-                self.loggerObj.debug('Telemetry reformatted (Serial: %s)', reformatted_telemetry['serial'])
-                # Optionally write the reformatted telemetry
-                if self.writer:
-                    self.writeData.write_reformatted_telemetry(self, reformatted_telemetry)
-                # Store the reformatted telemetry to the upload queue
-                try:
-                    self.upload_queue.put(reformatted_telemetry, False)
-                    self.loggerObj.debug('Reformatted telemetry put in queue (Serial: %s)', reformatted_telemetry['serial'])
-                except queue.Full:
-                    self.loggerObj.warning('Upload queue full')
+                    self.loggerObj.error('Mandatory data check failed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
             else:
-                self.loggerObj.error('Mandatory data check failed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
+                self.loggerObj.error('APRS package CRC failed')
 
 
 # Upload the telemetry packages
