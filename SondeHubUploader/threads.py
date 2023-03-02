ed74c78056e4cd02de779c549901e5cf6250938c
Simon Schäfer <34800304+Eshco93@users.noreply.github.com>
2023-03-02 01:03:51 +0100

    Update threads.py
    * Set socket in udp_receive to blocking operation
    * Set getting of APRS packages in process_aprs_queue to blocking operation

--- SondeHubUploader/threads.py
+++ SondeHubUploader/threads.py
@@ -15,69 +15,63 @@ import time
 def udp_receive(self):
     # Create a socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setblocking(False)
     sock.bind((self.addr, self.port))
     
     while self.running:
         # Try to receive an APRS package
+        data, addr = sock.recvfrom(self.shuConfig.udp_buffersize)
+        self.loggerObj.debug('APRS package received')
+        # Store the APRS package to the aprs queue
         try:
-            data, addr = sock.recvfrom(self.shuConfig.udp_buffersize)
-            self.loggerObj.debug('APRS package received')
-            # Store the APRS package to the aprs queue
-            try:
-                self.aprs_queue.put(data, False)
-                self.loggerObj.debug('APRS package put in queue')
-            except queue.Full:
-                self.loggerObj.warning('APRS package queue full')
-        # Nothing received
-        except socket.error:
-            pass
+            self.aprs_queue.put(data, False)
+            self.loggerObj.debug('APRS package put in queue')
+        except queue.Full:
+            self.loggerObj.warning('APRS package queue full')
 
 
 # Process the received APRS packages
 def process_aprs_queue(self):
     while self.running:
-        # Get packages, if there are any in the aprs queue
-        if not self.aprs_queue.empty():
-            aprs_package = self.aprs_queue.get(False)
-            self.loggerObj.debug('APRS package taken from queue')
-            # Optionally write the raw data
-            if self.writeo:
-                self.writeData.write_raw_data(self, aprs_package)
-            # Perform CRC
-            if self.crc_calculator.verify(aprs_package[:-2], aprs_package[-1] << 8 | aprs_package[-2]):
-                # Parse the APRS package
-                telemetry = self.handleData.parse_aprs_package(self, aprs_package[:-2])
-                self.loggerObj.debug('APRS package parsed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
-                self.loggerObj.info('Telemetry received (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
-                # Check whether the telemetry is plausible
-                telemetry = self.telemetryChecks.check_plausibility(self, telemetry)
-                self.loggerObj.debug('Plausibility checks performed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
-                # Optionally write the telemetry
-                if self.writet:
-                    if 'serial' in telemetry:
-                        self.writeData.write_telemetry(self, telemetry)
-                    else:
-                        self.loggerObj.error('Could not write telemetry (serial missing)')
-                # Check whether the mandatory telemetry for SondeHub is included
-                if self.telemetryChecks.check_mandatory(self, telemetry):
-                    self.loggerObj.debug('Mandatory data check successful (Serial: %s)', telemetry['serial'])
-                    # Reformat the telemetry to the SondeHub telemetry format
-                    reformatted_telemetry = self.handleData.reformat_telemetry(self, telemetry)
-                    self.loggerObj.debug('Telemetry reformatted (Serial: %s)', reformatted_telemetry['serial'])
-                    # Optionally write the reformatted telemetry
-                    if self.writer:
-                        self.writeData.write_reformatted_telemetry(self, reformatted_telemetry)
-                    # Store the reformatted telemetry to the upload queue
-                    try:
-                        self.upload_queue.put(reformatted_telemetry, False)
-                        self.loggerObj.debug('Reformatted telemetry put in queue (Serial: %s)', reformatted_telemetry['serial'])
-                    except queue.Full:
-                        self.loggerObj.warning('Upload queue full')
+        # Get package, if there are any in the aprs queue
+        aprs_package = self.aprs_queue.get()
+        self.loggerObj.debug('APRS package taken from queue')
+        # Optionally write the raw data
+        if self.writeo:
+            self.writeData.write_raw_data(self, aprs_package)
+        # Perform CRC
+        if self.crc_calculator.verify(aprs_package[:-2], aprs_package[-1] << 8 | aprs_package[-2]):
+            # Parse the APRS package
+            telemetry = self.handleData.parse_aprs_package(self, aprs_package[:-2])
+            self.loggerObj.debug('APRS package parsed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
+            self.loggerObj.info('Telemetry received (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
+            # Check whether the telemetry is plausible
+            telemetry = self.telemetryChecks.check_plausibility(self, telemetry)
+            self.loggerObj.debug('Plausibility checks performed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
+            # Optionally write the telemetry
+            if self.writet:
+                if 'serial' in telemetry:
+                    self.writeData.write_telemetry(self, telemetry)
                 else:
-                    self.loggerObj.error('Mandatory data check failed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
+                    self.loggerObj.error('Could not write telemetry (serial missing)')
+            # Check whether the mandatory telemetry for SondeHub is included
+            if self.telemetryChecks.check_mandatory(self, telemetry):
+                self.loggerObj.debug('Mandatory data check successful (Serial: %s)', telemetry['serial'])
+                # Reformat the telemetry to the SondeHub telemetry format
+                reformatted_telemetry = self.handleData.reformat_telemetry(self, telemetry)
+                self.loggerObj.debug('Telemetry reformatted (Serial: %s)', reformatted_telemetry['serial'])
+                # Optionally write the reformatted telemetry
+                if self.writer:
+                    self.writeData.write_reformatted_telemetry(self, reformatted_telemetry)
+                # Store the reformatted telemetry to the upload queue
+                try:
+                    self.upload_queue.put(reformatted_telemetry, False)
+                    self.loggerObj.debug('Reformatted telemetry put in queue (Serial: %s)', reformatted_telemetry['serial'])
+                except queue.Full:
+                    self.loggerObj.warning('Upload queue full')
             else:
-                self.loggerObj.error('APRS package CRC failed')
+                self.loggerObj.error('Mandatory data check failed (Serial: %s)', telemetry['serial'] if 'serial' in telemetry else 'N/A')
+        else:
+            self.loggerObj.error('APRS package CRC failed')
 
 
 # Upload the telemetry packages
